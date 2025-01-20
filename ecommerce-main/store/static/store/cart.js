ddd50122efad7a3ba33b2680be6fceb9058c309b
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2025-01-20 10:55:58 +0000

    docs: add comments to JS

--- ecommerce-main/store/static/store/cart.js
+++ ecommerce-main/store/static/store/cart.js
@@ -1,9 +1,16 @@
+/**
+ * Sets the item in the cart to the chosen quanity.
+ * 
+ * @param {string} name The name of the item.
+ * @param {number} quantity The chosen quantity.
+ */
 function setItem(name, quantity) {
     /** @type {Object|undefined} */
     let cart = JSON.parse(localStorage.getItem("cart"));
 
+    // If the cart is undefined that means the user has not made a cart yet
     if (cart == undefined) {
-        cart = {};
+        cart = {}; // So we make a new one
     }
 
     cart[name] = quantity;
@@ -11,28 +18,48 @@ function setItem(name, quantity) {
     localStorage.setItem("cart", JSON.stringify(cart));
 }
 
+
+/**
+ * Returns the quanity of a given item in the cart.
+ * 
+ * @param {string} name The name of the item.
+ * @returns {number|undefined} The item's quantity.
+ */
 function getItem(name) {
     /** @type {Object|undefined} */
     let cart = JSON.parse(localStorage.getItem("cart"));
 
+    // If the cart is undefined that means the user has not made a cart yet
     if (cart == undefined) {
-        cart = {};
+        cart = {}; // So we make a new one
     }
 
     return cart[name];
 }
 
+/**
+ * Removes the item from the cart.
+ * 
+ * @param {string} name The name of the item.
+ */
 function removeItem(name) {
     let cart = JSON.parse(localStorage.getItem("cart"));
 
+    // If the cart is undefined that means the user has not made a cart yet
     if (cart == undefined) {
-        cart = {};
+        cart = {}; // So we make a new one
     }
 
     delete cart[name];
     localStorage.setItem("cart", JSON.stringify(cart));
 }
 
+/**
+ * Increments the quanity of an item by 1.
+ * If the item does not exist in the cart its quanity will be set to 1.
+ * 
+ * @param {string} name The name of the item.
+ */
 function incItem(name) {
     let quantity = getItem(name);
     if (quantity == undefined) {
@@ -42,6 +69,13 @@ function incItem(name) {
     setItem(name, quantity);
 }
 
+/**
+ * Decrements the quanity of an item by 1.
+ * This function will ensure that the quanity cannot be less then 1.
+ * Also, if the item does not exist, the quanity will be set to 1.
+ * 
+ * @param {string} name The name of the item.
+ */
 function decItem(name) {
     let quantity = getItem(name);
     if (quantity == undefined) {
@@ -51,6 +85,12 @@ function decItem(name) {
     setItem(name, quantity);
 }
 
+/**
+ * Returns all items in the cart. The result object is not tied to the cart,
+ * which results in modifactions being ignored.
+ * 
+ * @returns {object} An object whos keys represent item names and values represent the quanity.
+ */
 function getItems() {
     /** @type {Object|undefined} */
     let cart = JSON.parse(localStorage.getItem("cart"));
@@ -63,8 +103,13 @@ function getItems() {
     return cart;
 }
 
+/**
+ * Updates an element with the ID `"cart"` to contain a table which represents all items in the cart.
+ */
 function renderCart() {
-    let cart = getItems();
+    let cart = getItems(); // Get the all items in the cart
+
+    // Construct a string to represent the HTML for a table, first start with the header.
     let table = `
         <table border='1'>
             <thead>
@@ -78,15 +123,18 @@ function renderCart() {
             <tbody>
     `;
 
+    // Initialise all totals to 0
     let totalItems = 0;
     let totalCost = 0;
 
+    // Iterate through all items in the cart
     for (let item in cart) {
-        let quantity = cart[item];
-        let cost = getItemCost(item);
-        totalItems += quantity;
-        totalCost += cost * quantity;
+        let quantity = cart[item]; // Get the item quanity
+        let cost = getItemCost(item); // Get item price
+        totalItems += quantity; // Append to to total
+        totalCost += cost * quantity; // ^^^
 
+        // Append a new row to the table displaying item properties
         table += `
             <tr>
                 <td>${quantity}</td>
@@ -101,6 +149,7 @@ function renderCart() {
         `;
     }
 
+    // Finish the table by appending HTML to close the table and include a footer with totals
     table += `
             </tbody>
             <tfoot>
@@ -116,14 +165,28 @@ function renderCart() {
         </table>
     `;
 
+    // Overwrite an HTML element with ID of "cart" to contain the new table
     document.getElementById("cart").innerHTML = table;
 }
 
-// Example function to get the cost of an item
+/**
+ * Fetchs and item price fro, a object located at `window.prices` whos keys represent 
+ * item names and values represent prices.
+ * 
+ * @param {string} item The name of the item.
+ * @returns {number} Returns the item's price, or 0 if one does not exist.
+ */
 function getItemCost(item) {
     return window.prices[item] || 0;
 }
 
+/**
+ * Takes a quanity from an input element with id `"quanity"` and sets an item using it.
+ * This function will alert the user if the quanity is less then 1 and will alert when the
+ * item is added to the cart.
+ * 
+ * @param {string} item The name of an item.
+ */
 function subbmitItem(item) {
     const quantity = Number(document.getElementById("quanity").value);
 
