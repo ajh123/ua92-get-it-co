71000c8b8493a63a4f093f7157defff684852b41
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2025-01-23 09:48:15 +0000

    docs: add more comments to CSS and JS

--- ecommerce-main/store/static/store/cart.js
+++ ecommerce-main/store/static/store/cart.js
@@ -5,7 +5,8 @@
  * @param {number} quantity The chosen quantity.
  */
 function setItem(name, quantity) {
-    /** @type {Object|undefined} */
+    // Fetch the cart from the browser's local storage,
+    // then convert it into a JavaScript object using JSON.
     let cart = JSON.parse(localStorage.getItem("cart"));
 
     // If the cart is undefined that means the user has not made a cart yet
@@ -13,8 +14,11 @@ function setItem(name, quantity) {
         cart = {}; // So we make a new one
     }
 
+    // Set the quanity of the by assinging by indexing the cart with "name"
     cart[name] = quantity;
 
+    // Convert the updated cart object back to a JSON string and store it in the
+    // browser's local storage
     localStorage.setItem("cart", JSON.stringify(cart));
 }
 
@@ -34,6 +38,8 @@ function getItem(name) {
         cart = {}; // So we make a new one
     }
 
+    // Then we index the cart object with "name" to fect the current value,
+    // this may return `undefined` if the cart does not contian the item.
     return cart[name];
 }
 
@@ -43,6 +49,8 @@ function getItem(name) {
  * @param {string} name The name of the item.
  */
 function removeItem(name) {
+    // Fetch the cart from the browser's local storage,
+    // then convert it into a JavaScript object using JSON.
     let cart = JSON.parse(localStorage.getItem("cart"));
 
     // If the cart is undefined that means the user has not made a cart yet
@@ -50,7 +58,11 @@ function removeItem(name) {
         cart = {}; // So we make a new one
     }
 
+    // The "delete" keyword can be used remove any data from the program include object members
     delete cart[name];
+
+    // Convert the updated cart object back to a JSON string and store it in the
+    // browser's local storage
     localStorage.setItem("cart", JSON.stringify(cart));
 }
 
@@ -61,11 +73,15 @@ function removeItem(name) {
  * @param {string} name The name of the item.
  */
 function incItem(name) {
+    // We fetch the item using our `getItem` method
     let quantity = getItem(name);
+    // If the quanity is `undefined` then we set the quanity to 0
     if (quantity == undefined) {
         quantity = 0;
     }
+    // Then increment the quanity
     quantity += 1;
+    // Then use `setItem` to update the item's quanity in the cart
     setItem(name, quantity);
 }
 
@@ -77,11 +93,15 @@ function incItem(name) {
  * @param {string} name The name of the item.
  */
 function decItem(name) {
+     // We fetch the item using our `getItem` method
     let quantity = getItem(name);
+    // If the quanity is `undefined` then we set the quanity to 1
     if (quantity == undefined) {
         quantity = 1;
     }
+    
     quantity = Math.max(1, quantity - 1);
+    // Then use `setItem` to update the item's quanity in the cart
     setItem(name, quantity);
 }
 
@@ -89,16 +109,22 @@ function decItem(name) {
  * Returns all items in the cart. The result object is not tied to the cart,
  * which results in modifactions being ignored.
  * 
+ * This function makes a new cart instance if one does not exist.
+ * 
  * @returns {object} An object whos keys represent item names and values represent the quanity.
  */
 function getItems() {
     /** @type {Object|undefined} */
+    // Fetch the cart from the local storage again and convert it to an object with JSON
     let cart = JSON.parse(localStorage.getItem("cart"));
 
+    // If the cart is `undefined` meaning it does not exist, then we create a new one
     if (cart == undefined) {
         cart = {};
     }
 
+    // If we have created a new cart then we put it in the browser's local storage.
+    // This will just do nothing if there is already a cart.
     localStorage.setItem("cart", JSON.stringify(cart));
     return cart;
 }
@@ -110,6 +136,8 @@ function renderCart() {
     let cart = getItems(); // Get the all items in the cart
 
     // Construct a string to represent the HTML for a table, first start with the header.
+    // As we can see there is the excat HTML definition for a table with each element inside
+    // as expected.
     let table = `
         <table border='1'>
             <thead>
@@ -135,6 +163,10 @@ function renderCart() {
         totalCost += cost * quantity; // ^^^
 
         // Append a new row to the table displaying item properties
+        // A table row in HTML is a `<tr>` and each colloumn in the row is a `<td>`
+        // Then by using the syntax `${quantity}` we can insert the varible content in the HTML string.
+        //
+        // We append to the existing table string since we want one object to represent the whole table.
         table += `
             <tr>
                 <td>${quantity}</td>
@@ -177,6 +209,10 @@ function renderCart() {
  * @returns {number} Returns the item's price, or 0 if one does not exist.
  */
 function getItemCost(item) {
+    // This code assumes that `window.prices` is a JavaScript object that already exists in global memeory,
+    // which in `profile.html` this global object is created.
+    // Using the OR operation (`||`) will ensure that if `window.prices` is undefined that a value is stil
+    // returned, in this case 0.
     return window.prices[item] || 0;
 }
 
@@ -188,13 +224,22 @@ function getItemCost(item) {
  * @param {string} item The name of an item.
  */
 function subbmitItem(item) {
+    // This looks for a HTML element with the ID of `quanity`, this only exists in `product.html` and in that
+    // file there is a form input with the ID of `quanity`. 
+    // When the user subbmits that form, this function is called and this line will fetch the value of the input.
     const quantity = Number(document.getElementById("quanity").value);
 
+    // We check if the quanity is less then 1, at runtime this is impossible as the form input as a minimun of 1, but
+    // an attacker could inject code into the browser console and submit less then 1.
     if (quantity < 1) {
+        // We use `alert` to show a dialog to the user.
         alert("Item quanity must be less than 1!")
     } else {
-        const currQuant = getItem(item) | 0;
+        // We fetch the current item and or if the result is undefined we set it to 1
+        const currQuant = getItem(item) || 1;
+        // We then put the item into the cart
         setItem(item, currQuant + quantity);
+        // And alert to the user this operation was successfull
         alert(`${quantity} X ${item} added to cart.`)
     }
 }
