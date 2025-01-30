7bbd7f4fa3db25643a82096ab6c7f8a3d1e61710
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2025-01-30 12:20:11 +0000

    feat: working login / signup

--- backend/views.ts
+++ backend/views.ts
@@ -3,6 +3,9 @@ import { type ViewArgs, type Route } from "./utils";
 
 import { authenticate, getProductById, getProducts, getUser } from "./storage";
 import { generateSessionId, setSession, getCurrentUser, logoutUser } from "./session";
+import { isValidEmail } from "./validation";
+import type { User } from "./storage/types";
+import { users } from "./storage/dummy";
 
 // HTML VIEWS
 
@@ -52,6 +55,10 @@ export async function profile(req: Request) {
     const user = getCurrentUser(req);
     const is_authenticated = user != undefined;
 
+    if (!is_authenticated) {
+        return Response.redirect("/login")
+    }
+
     const data = {
         user: user,
         is_authenticated: is_authenticated
@@ -63,6 +70,10 @@ export async function profile(req: Request) {
 export async function login(req: Request) {
     const user = getCurrentUser(req);
     const is_authenticated = user != undefined;
+
+    if (is_authenticated) {
+        return Response.redirect("/")
+    }
 
     const data = {
         user: user,
@@ -102,6 +113,74 @@ export async function login(req: Request) {
     return render("./frontend/login.html", data);
 }
 
+export async function signup(req: Request) {
+    const user = getCurrentUser(req);
+    const is_authenticated = user != undefined;
+
+    if (is_authenticated) {
+        return Response.redirect("/")
+    }
+
+    const data = {
+        user: user,
+        is_authenticated: is_authenticated,
+        errors: [] as String[]
+    };
+
+    if (req.method == "POST") {
+        const formData = await req.formData();
+
+        const name = formData.get("name")?.toString() || "";
+        const email = formData.get("email")?.toString() || "";
+        const password = formData.get("password")?.toString() || "";
+        const confirm_password = formData.get("confirm_password")?.toString() || "";
+
+        if (name.length < 3) {
+            data.errors.push("Name must contain more then 3 characters.");
+        }
+
+        if (email.length < 3) {
+            data.errors.push("Email must contain more then 3 characters.");
+        }
+        if (!isValidEmail(email)) {
+            data.errors.push("Email must be valid.");
+        }
+
+        if (password.length < 3 || confirm_password.length < 3) {
+            data.errors.push("Password must contain more then 3 characters.");
+        }
+        if (confirm_password != password) {
+            data.errors.push("Confirm Password and Password must match");
+        }
+
+        if (data.errors.length > 0) {
+            return render("./frontend/signup.html", data);
+        }
+
+        const newUser: User = {
+            name: name,
+            email: email,
+            orders: [],
+            password: password
+        }
+
+        users.push(newUser);
+
+        const sessionId = generateSessionId();
+        setSession(sessionId, newUser);
+        const response = new Response(null, {
+            status: 302,
+            headers: {
+                'Set-Cookie': `sessionId=${sessionId};`,
+                'Location': '/'
+            }
+        });
+        return response;
+    }
+
+    return render("./frontend/signup.html", data);
+}
+
 export async function logout(req: Request) {
     logoutUser(req);
     return Response.redirect("/")
@@ -123,6 +202,7 @@ export const routes: Record<string, Route>  = {
     "/products": product_index,
     "/products/:id": product,
     "/login": login,
+    "/signup": signup,
     "/logout": logout,
     "/profile": profile,
 };
