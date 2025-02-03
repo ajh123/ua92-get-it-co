be193a85aaf5f4567f81f840733461cefd869d9c
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2025-02-03 13:13:27 +0000

    feat: add profile API endpoint and update profile form for user details submission

---
+++ src/pages/api/v1/profile.ts
@@ -0,0 +1,25 @@
+import type { APIRoute } from 'astro';
+
+export const POST: APIRoute = async ({ request, cookies }) => {
+    if (!cookies.has("access_token")) {
+        return new Response('Unauthorized - No token found', { status: 401 });
+    }
+
+    const access_token = cookies.get('access_token'); // Access token from cookie
+
+    // Extract form data from the request body
+    const formData = new URLSearchParams(await request.text());
+
+    // Forward the request to the auth server
+    await fetch('https://authserver.minersonline.uk/userinfo', {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${access_token.value}`,
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: formData, // Forward the original body to the auth server
+    });
+
+    // Return the user back to their profile
+    return Response.redirect("/profile", 301)
+};
