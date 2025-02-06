44c2514bc15ce6c309fc8a1b78b8da762fc212e5
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2025-02-06 09:11:32 +0000

    fix: only use cloudflare adapter in production

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,22 @@
 import { defineConfig } from 'astro/config';
 
 import cloudflare from "@astrojs/cloudflare";
+import node from "@astrojs/node"
+
+const isProd = import.meta.env.PROD;
+const isDev = import.meta.env.DEV;
 
 // https://astro.build/config
-export default defineConfig({
-  output: "server",
-  adapter: cloudflare(),
-});
+let config = {
+  output: "server"
+}
+
+if (isProd) {
+  config.adapter = cloudflare()
+} else {
+  config.adapter = node({
+    mode: 'standalone',
+  })
+}
+
+export default defineConfig(config);
